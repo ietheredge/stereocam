7099b15c480a070b61b70dce3b3c50103638247a
robertian <etheredge@utexas.edu>
2015-10-15 15:25:17 -0500

    starting point for data acquisition (camera, adafruit DOF IMU/temp, preassure and RTC)

--- batcheck.py
+++ batcheck.py
@@ -4,7 +4,7 @@ class App:
 
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(17, GPIO.IN)
+        GPIO.setup(16, GPIO.IN)
         print 'pin 16: ',str(GPIO.input(16))
 
     def check(self):
