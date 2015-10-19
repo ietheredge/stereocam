a65052eca10c90837f89ff6499ab34f9eead4fa3
robertian <etheredge@utexas.edu>
2015-10-19 17:30:13 -0500

    building up camera module, still missing a lot of functionality.

--- camera.py
+++ camera.py
@@ -1,30 +1,48 @@
 import picamera
 import logging
+import argparse
+import time
+import io
+
 
 class App:
 
-    def __init__(self):
-        batlog = logging.getLogger('lowbatlog')
-        hdlr = logging.FileHandler('log/lowbatlog.log')
-        formatter = logging.Formatter('%(asctime)s, %(levelname)s, %(message)s', "%m/%d/%Y:%H:%M:%S")
+    def __init__(self, imformat, itterations, outputfile):
+        # setup log
+        datlog = logging.getLogger('datalog')
+        hdlr = logging.FileHandler('log/datalog.log')
+        formatter = logging.Formatter('%(asctime)s, %(levelname)s, %(message)s', "%m/%d/%Y:%H:%M:%S.%f")
         hdlr.setFormatter(formatter)
-        batlog.addHandler(hdlr)
-        batlog.setLevel(logging.INFO)
-        batlog.info("battery voltage drop detected")
+        datlog.addHandler(hdlr)
+        datlog.setLevel(logging.INFO)
+        datlog.info("camera initiated stack n= ",str(itterations)," format= ",str(imformat)," output to=",str(outputfile))
+        self.imformat = imformat
+        self.n = itterations
+        self.out = outputfile
 
 
-    def capimagestack(self, format):
-        camera.exposure_mode = 'sports'
+    def capimage(self):
+        pass
+
+    def capimagestack(self):
+        with picamera.PiCamera() as camera:
+            time.sleep(2)
+            camera.exposure_mode = 'sports'
             logging.info('camera exposure setting:'+str(camera.exposure_mode))
             logging.info('camera shutter speed:'+str(camera.shutter_speed))
-            start = time.time()
-            camera.capture_sequence((
-                'img%04d.jpg' % i
-                for i in range(30)
-            ), use_video_port=False)
+            camera.capture_sequence((str(self.out),'_%04d.jpg' % i for i in range(self.n)), use_video_port=False)
 
+    def capvideo(self):
+        pass
+
+    def capRAW(self):
+        stream = io.BytesIO()
+        with picamera.PiCamera() as camera:
+            time.sleep(2)
+            camera.capture(stream, format='jpeg', bayer=True)
 
 if __name__=='__main__':
+
     ap = argparse.ArgumentParser()
     ap.add_argument("-s", "--stack", help="capture image stack, takes no input", action='store_true')
     ap.add_argument("-v", "--video", help="capture video, takes no input", action='store_true')
@@ -34,4 +52,17 @@ if __name__=='__main__':
     ap.add_argument("-l", "--lenght", help="pass video length")
     ap.add_argument("-o", "--output", help="output file name")
     args = vars(ap.parse_args())
-    kamera = App()
+
+    kamera = App(args["format"],args["number"],args["output"])
+
+    if args["image"]:
+        if args["format"]=='RAW':
+            kamera.capRAW()
+        else:
+            kamera.capimage()
+    if args["video"]:
+        kamera.capvideo()
+    if args["stack"]:
+        kamera.capimagestack()
+
+
