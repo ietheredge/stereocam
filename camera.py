59efd061b8458fca4cc4a9c747fe1c006f988165
robertian <1qa2ws3ed>
2015-10-24 10:40:52 -0500

    converted format string to tuple

--- camera.py
+++ camera.py
@@ -21,7 +21,7 @@ class App:
 
         # setup camera
         self.camera = picamera.PiCamera()
-        self.camera.resolution = tuple(int(item) for item in exposure.split('x') if item.strip())
+        self.camera.resolution = tuple(int(item) for item in res.split('x') if item.strip())
         self.imformat = imformat
         self.camera.exposure_mode = exposure
         self.camera.framerate = int(rate)
