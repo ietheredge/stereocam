a921f0c49ea5010d307da62a02d48705e8bf245f
robertian <1qa2ws3ed>
2015-10-27 14:47:47 -0500

    troubleshooting continues

--- camera.py
+++ camera.py
@@ -46,7 +46,7 @@ class App:
 
     def capraw(self):
         #stream = io.BytesIO()
-        self.camera.capture('../data/%(asctime)s.jpg' % 'H-%M-%S-%f' , format='jpeg', bayer=True)
+        self.camera.capture('../data/%(asctime)s.jpg' % "H-%M-%S-%f" , format='jpeg', bayer=True)
 
     def capcontinuous(self):
         stream = io.BytesIO()
