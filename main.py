db29b0a8b5916b64cbdb05bedafdd6a0f8ebf819
robertian <1qa2ws3ed>
2015-10-27 15:52:34 -0500

    troubleshooting continues

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ s = RTIMU.Settings(SETTINGS_FILE)
 if not os.path.exists(SETTINGS_FILE + ".ini"):
     logging.info('Settings file does not exist, will be created')
 imu = RTIMU.RTIMU(s)
-
+temp = RTIMU.RTPressure(s)
 
 logging.info('IMU Name: %f' % imu.IMUName())
 if (not imu.IMUInit()):
@@ -32,10 +32,10 @@ kamera = camera.App('yuv', 'yuv' '1920x1080', 'sports', '30', '1', 'outfile')
 if imu.IMURead():
     kamera.capraw()
     data = imu.getIMUData()
-    logging.info(data["pressureValid"], data["pressure"], data["temperatureValid"], data["temperature"]) = pressure$
+    (na, na2, na3, data["temperature"]) = temp.pressureRead()
     fusionPose = data["fusionPose"]
-    logging.info("r: %f p: %f y: %f" % (math.degrees(fusionPose[0]),
-    math.degrees(fusionPose[1]), math.degrees(fusionPose[2])))
+    logging.info("r: %f p: %f y: %f" % (math.degrees(fusionPose[0]), math.degrees(fusionPose[1]),
+                                        math.degrees(fusionPose[2])))
     if (data["pressureValid"]):
         logging.info("Pressure: %f" % data["pressure"])
     if (data["temperatureValid"]):
