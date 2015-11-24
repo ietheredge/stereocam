3715e8f7bff00ad128faa132d158e498f6f12771
robertian <1qa2ws3ed>
2015-11-24 12:49:05 -0600

    troubleshooting fix

--- python/mainLIGHT.py
+++ python/mainLIGHT.py
@@ -46,5 +46,5 @@ while True:
             (data["pressureValid"], data["pressure"], data["temperatureValid"], data["temperature"]) = temp.pressureRead()
             fusionPose = data["fusionPose"]
             camera.capture('../data/'+str(datetime.datetime.now().strftime('%H-%M-%S-%f'))+str("_%f" % data["temperature"])+str("_%f-%f-%f_%s_%f_%f" % (math.degrees(fusionPose[0]), math.degrees(fusionPose[1]),
-                                            math.degrees(fusionPose[2])), ('I' if intosunx==True else 'A' if awayfromsun==True else 'P'sunalt, sunaz)+'.jpg' , format='jpeg', bayer=True)
+                                            math.degrees(fusionPose[2])), ('I' if intosunx==True else 'A' if awayfromsun==True else 'P'), sunalt, sunaz)+'.jpg' , format='jpeg', bayer=True)
         time.sleep(poll_interval*1.0/1000.0)
