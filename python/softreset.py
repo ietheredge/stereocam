58a2cae576154d45dfa32ac82930faafb597768e
robertian <etheredge@utexas.edu>
2015-12-17 12:41:05 -0600

    ...

--- python/softreset.py
+++ python/softreset.py
@@ -9,12 +9,12 @@ class App():
         self.led1 = 17
         self.led2 = 27
         self.led3 = 22
-        GPIO.setup(self.pin, GPIO.IN)
+        GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.led1, GPIO.OUT)
-        GPIO.setup(self.led1, GPIO.OUT)
-        GPIO.setup(self.led1, GPIO.OUT)
+        GPIO.setup(self.led2, GPIO.OUT)
+        GPIO.setup(self.led3, GPIO.OUT)
         GPIO.remove_event_detect(self.pin)
-        GPIO.add_event_detect(self.pin, GPIO.BOTH, callback=App.shutitdown(self))
+        GPIO.add_event_detect(self.pin, GPIO.FALLING, callback=App.shutitdown(self))
 
 
 
